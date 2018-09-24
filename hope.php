4a38894b62d9a075768b68715df0a9eaa3dcdf17
diarrabell <dbell5@u.rochester.edu>
2018-09-24 16:09:31 -0400

    edited hope

--- hope.php
+++ hope.php
@@ -8,19 +8,22 @@
 	
 		<img src="images/portrait.jpg" alt = "Hope Barkley">
 		
-		<ul class="list">
+        <ul class="list">
 			<li> Digital Media Studies </li>
 			<li> Computer Science </li>
 			<li> Business </li>
 		</ul>	
 
 	<div>
+        
+        <h2>Intro</h2>
 		<p>
 			 Hi, my name is Hope Barkley and I am a digital media studies student at the University of Rochester. I grew up in Tarrytown, NY, a suburb about 30 minutes outside of New York City. I attended Solomon Schechter School of Westchester, a Jewish day school located in Hartsdale, New York. I started gymnastics when I was 7 years-old and was a competetive gymnast throughout my high school career, before deciding not to pursue it in college. Before going to college, I reached USAG level 9 gymnastics and competed on a State and Regional level. During my senior year of high school I applied Early Decision to the University of Rochester and was accepted! I am currently majoring in Digital Media Studies, an interdisciplinary major focussing on technology industries, and hope to pursue a career in design or web development post graduation. 
 		</p>
 	</div>
 
 	<div>
+        <h2>Internships and Work Experience</h2>
 	<p>
 		I have had a variety of internships and jobs throughout my life. My first job was as a gymnastics coach at Westchester Gymnastics, where I taught to beginners. I spent the summer of 2016 working as the gymnastics coordinator for a day camp run by the Jewish Community Center of Tarrytown, where I worked with over 300 children every day teaching basic gymnastics skills. In the summer of 2017 following my Freshmen year of college, I worked in the User Experience Design department of Mastercard in NYC, and got the chance to work on a variety of exciting projects such as the redesign of MC's website, apps and conduct multiple research tests. This past summer, I was a graphic design intern for Regeneron, a biotechnology company located in Tarrytown NY. In this position, I helped run and manage many of their social media campaigns, create designs for corporate materials, and learned a great deal about marketing within the heathcare industry.  
 	</p>
@@ -33,6 +36,7 @@
 	</div>
 
 	<div>
+        <h2>Courses</h2>
 	<p>
 		At University of Rochester, I have taken many classes to help advance my skillsets and knowledge. I have completed courses such as DMS 101, DMS 102, DMS 103, and DMS 104 to ensure that I am prepared for upper level DMS classes. Additionally, I am one course away from completion of my computer science minor, and now have HTML, Python and Java skills. I am currently enrolled in many Business courses, such as Finance, and have previously taken multiple Economics and accounting classes.
 	</p>
@@ -40,12 +44,14 @@
 
 	<footer>
 		<p> CSC 174 Team NYC </p>
+<!--
 
 		<ul class="list2"> 
        		 <li> <a href="http://csc174.org/assignment01/jmyers/lab_01/">Julia's page</a></li> 
        		 <li> <a href="http://csc174.org/assignment01/hbarkley/">Hope's page</a> </li> 
       		 <li> <a href="http://csc174.org/assignment01/bsmart/Assignment%201/">Brandon's page</a></li> 
    		</ul>
+-->
 	</footer>
 	
 	<script src="http://code.jquery.com/jquery.js"></script>
