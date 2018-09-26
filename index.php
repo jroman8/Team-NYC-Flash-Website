fb62bfb1b351acfe0a4ee353e74d8958d7decb0f
diarrabell <dbell5@u.rochester.edu>
2018-09-26 15:00:01 -0400

    made edits

--- index.php
+++ index.php
@@ -7,11 +7,19 @@
 <h1>TEAM NYC</h1>
 
 	<?php include "include/nav.inc"; ?>
-
 <ul class="pics">
-    <li><a href="hope.php"><img src="images/portrait.jpg" alt = "Hope"></a></li>
-    <li><a href=brandon.php><img src="images/me.jpg" alt = "Brandon"></a></li>
-    <li><a href="julia.php"><img src="images/julia.jpg" alt = "Julia"></a></li>
+    <li>
+        
+        <a href="julia.php"><img src="images/julia.jpg" alt = "Julia"></a>
+        
+    </li>
+    <li>
+        <a href="hope.php"><img src="images/portrait.jpg" alt = "Hope"></a>
+    </li>
+    <li>
+        <a href=brandon.php><img src="images/me.jpg" alt = "Brandon"></a>
+
+    </li>  
 </ul>
 	
 <script src="http://code.jquery.com/jquery.js"></script>
